396bb1299783463f8785de24d78e3429653ca210
Ruben Laguna <ruben.laguna@ericsson.com>
2013-07-30 22:39:34 +0200

    Add fish_prompt and fish_right_prompt

--- fishfunctions/fish_prompt.fish
+++ fishfunctions/fish_prompt.fish
@@ -1,7 +1,10 @@
-function fish_prompt
-  set_color purple
-  date "+%m/%d/%y"
-  set_color red
-  echo (pwd) '>'
-  set_color normal
-end
+#function fish_prompt
+#  set_color purple
+#  date "+%m/%d/%y"
+#  set_color red
+#  echo (pwd) (whoami) '>'
+#  set_color normal
+#end
+function fish_prompt -d "Write out the left prompt"
+  printf "%s >" (prompt_pwd)
+end 
---
+++ fishfunctions/fish_right_prompt.fish
@@ -0,0 +1,4 @@
+function fish_right_prompt
+  #echo (date '+%H:%M:%S')
+  date '+%H:%M:%S'
+end
