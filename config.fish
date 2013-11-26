0aa23f146a19d48c32216ac76a23e10982a8f508
Ruben Laguna <ruben.laguna@ericsson.com>
2013-11-26 22:12:38 +0100

    Add default CFLAGS to fish

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 rvm > /dev/null
 set -gx PATH ~/bin /usr/local/bin $PATH
 set -gx LC_CTYPE "en_US.UTF-8"
+set -xU CFLAGS "-g -Wall -Werror -O3 -std=gnu11"
 set BROWSER 'open' # needed for help command to work
 set EDITOR vim
