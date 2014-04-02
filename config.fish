98ebdca8cca40c67c182fbae54db66fc1686f945
Ruben Laguna <ruben.laguna@ericsson.com>
2014-04-02 21:57:28 +0200

    fish: Remove CFLAGS

--- config.fish
+++ config.fish
@@ -1,6 +1,5 @@
 rvm > /dev/null
 set -gx PATH ~/bin /usr/local/bin /usr/local/sbin $PATH
 set -gx LC_CTYPE "en_US.UTF-8"
-set -xU CFLAGS "-g -Wall -Werror -O3 -std=gnu11"
 set BROWSER 'open' # needed for help command to work
 set EDITOR vim
