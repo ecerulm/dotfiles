e1839d0a6de9ced6b6f14d033e2cdba281dca322
Ruben Laguna <ruben.laguna@ericsson.com>
2014-04-08 23:55:45 +0200

    fish: get rid of annoying warning rvm_prompt

--- fishfunctions/rvm_prompt.fish
+++ fishfunctions/rvm_prompt.fish
@@ -1,7 +1,7 @@
 function rvm_prompt
   set -l yellow (set_color -o yellow)
   if type -t rvm >/dev/null
-    set -l identifier (rvm tools identifier)
+    set -l identifier (rvm tools identifier ^/dev/null)
     echo "$yellow$identifier "
   else
     echo ""
