0c8e0a62c9ab0985a84bf00c3539d1d2969e05df
Ruben Laguna <ruben.laguna@gmail.com>
2014-04-30 11:42:05 +0200

    gitconfig: include a platform specific file
    Plaform specific options are included through
    .gitconfig_platform_specific which is a symlink to the the appropiate
    file .gitconfig_osx or .gitconfig_linux.
    
    The symlink is created by the create_links*.sh scripts

--- create_links_linux.sh
+++ create_links_linux.sh
@@ -2,3 +2,5 @@
 ls -A | grep "^\." | grep -v "^\.git$" | xargs -n1 -I'{}' ln -Tvfs ~/dotfiles/'{}' ~/'{}'
 mkdir -p ~/bin
 ls bin | xargs -n1 -I'{}' ln -Tvfs ~/dotfiles/bin/'{}' ~/bin/'{}'
+#create system specific symlinks
+ln -Tvfs ~/dotfiles/.gitconfig_linux .gitconfig_platform_specific
--- create_links_mac.sh
+++ create_links_mac.sh
@@ -2,3 +2,5 @@
 ls -A | grep "^\." | grep -v "^\.git$" | xargs -n1 -I'{}' ln -Fvhfs ~/dotfiles/'{}' ~/'{}'
 mkdir -p ~/bin
 ls bin | xargs -n1 -I'{}' ln -Fvhfs ~/dotfiles/bin/'{}' ~/bin/'{}'
+#create system specific symlinks
+ln -Fvhfs ~/dotfiles/.gitconfig_osx .gitconfig_platform_specific
