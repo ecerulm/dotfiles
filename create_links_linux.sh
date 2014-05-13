072695b16402955e4456e808b2c60517ca095f36
Ruben Laguna <ruben.laguna@gmail.com>
2014-05-13 13:23:18 +0200

    Symlink the fish functions dir

--- create_links_linux.sh
+++ create_links_linux.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# dotfiles in the home dir
 ls -A | grep "^\." | grep -v "^\.git$" | xargs -n1 -I'{}' ln -Tvfs ~/dotfiles/'{}' ~/'{}'
+
+# Fish config files
+mkdir -p ~/.config/fish/
+ln -Tvfs ~/dotfiles/fishfunctions ~/.config/fish/functions
+
+# bin 
 mkdir -p ~/bin
 ls bin | xargs -n1 -I'{}' ln -Tvfs ~/dotfiles/bin/'{}' ~/bin/'{}'
+
 #create system specific symlinks
 ln -Tvfs ~/dotfiles/.gitconfig_linux .gitconfig_platform_specific
 ln -Tvfs ~/dotfiles/.gitconfig_linux ~/.gitconfig_platform_specific
