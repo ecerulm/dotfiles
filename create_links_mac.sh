1f04111f52188ed92157d0b006fd33931f6bfbb9
Ruben Laguna <ruben.laguna@gmail.com>
2014-05-20 09:57:11 +0200

    Update create_links_mac to set config.fish

--- create_links_mac.sh
+++ create_links_mac.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# dotfiles in the home dir
 ls -A | grep "^\." | grep -v "^\.git$" | xargs -n1 -I'{}' ln -Fvhfs ~/dotfiles/'{}' ~/'{}'
+
+# Fish config files
+mkdir -p ~/.config/fish/
+ln -Fvhfs ~/dotfiles/fishfunctions ~/.config/fish/functions
+ln -Fvhfs ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+#bin 
 mkdir -p ~/bin
 ls bin | xargs -n1 -I'{}' ln -Fvhfs ~/dotfiles/bin/'{}' ~/bin/'{}'
+
 #create system specific symlinks
 ln -Fvhfs ~/dotfiles/.gitconfig_osx .gitconfig_platform_specific
 ln -Fvhfs ~/dotfiles/.gitconfig_osx ~/.gitconfig_platform_specific
