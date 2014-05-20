3a78d4c6696d1a4886356edd80ad52529231d837
Ruben Laguna <ruben.laguna@gmail.com>
2014-05-20 09:56:42 +0200

    Get rid of rvm warning about path in fish

--- fishfunctions/rvm.fish
+++ fishfunctions/rvm.fish
@@ -1,7 +1,7 @@
 function rvm --description='Ruby enVironment Manager'
   # run RVM and capture the resulting environment
   set --local env_file (mktemp -t rvm.fish.XXXXXXXXXX)
-  bash -c 'source ~/.rvm/scripts/rvm; rvm "$@"; status=$?; env > "$0"; exit $status' $env_file $argv
+  bash -lc 'source ~/.rvm/scripts/rvm; rvm "$@" ; status=$?; env > "$0"; exit $status' $env_file $argv
 
   # apply rvm_* and *PATH variables from the captured environment
   and eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' | sed '/^[^=]*PATH/y/:/ /; s/^/set -xg /; s/=/ /; s/$/ ;/; s/(//; s/)//')
