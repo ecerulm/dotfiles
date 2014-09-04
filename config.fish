4ab36e60a80307309f9b31b48bcde38d6706ba64
Ruben Laguna <ruben.laguna@gmail.com>
2014-09-04 10:17:40 +0200

    fish: Reset the PATH to sensible defaults

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
-rvm > /dev/null
-set -gx PATH ~/bin /usr/local/bin /usr/local/sbin $PATH
+set -gx BASE_PATH /usr/sbin /usr/bin /sbin /bin
+set -gx PATH ~/bin /usr/local/bin /usr/local/sbin $BASE_PATH
 set -gx LC_CTYPE "en_US.UTF-8"
 set BROWSER 'open' # needed for help command to work
 set EDITOR vim
