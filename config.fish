5eed4d0e591c04ec9b8236e04c1250e8dec6cced
Ruben Laguna <ruben.laguna@gmail.com>
2014-09-26 18:19:00 +0200

    fish: update EDITOR and PATH

--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 #set PATH_ORIG $PATH
 #set -gx BASE_PATH /usr/sbin /usr/bin /sbin /bin
 #set -gx PATH ~/bin /usr/local/bin /usr/local/sbin $BASE_PATH
+if status --is-interactive
+  set PATH /usr/local/bin /usr/local/sbin $PATH
+end
 set -gx LC_CTYPE "en_US.UTF-8"
 set BROWSER 'open' # needed for help command to work
-set EDITOR vim
+set -gx EDITOR vim
