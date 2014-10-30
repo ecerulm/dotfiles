3c24d495f3060758ad77544e2e406061ec4a9c0e
Ruben Laguna <ruben.laguna@gmail.com>
2014-10-30 13:16:29 +0100

    Add python autocompletion by default

--- config.fish
+++ config.fish
@@ -7,6 +7,7 @@ end
 set -gx LC_CTYPE "en_US.UTF-8"
 set BROWSER 'open' # needed for help command to work
 set -gx EDITOR vim
+set -gx PYTHONSTARTUP ~/.pythonrc
 switch (uname)
   case Linux
     alias pbcopy='xsel --clipboard --input'
