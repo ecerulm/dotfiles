f15e1b00e113f26a54252ada629de4350367f06e
Ruben Laguna <ruben.laguna@gmail.com>
2014-11-13 15:53:59 +0100

    fish: more robust vim alias
    It will only add the -S Session.vim if we don't provide arguments

--- fishfunctions/vim.fish
+++ fishfunctions/vim.fish
@@ -1,7 +1,12 @@
 function vim
-	set -l vim_session_opt ''
-	if test -e Session.vim
-		set vim_session_opt '-S' 'Session.vim'
-	end
-	command vim $vim_session_opt $argv
+	if test -z $argv
+    # only if no arguments provided
+    if test -e Session.vim
+      command vim -S Session.vim
+    else
+      command vim $argv
+    end
+  else
+      command vim $argv
+  end
 end
