38fc3a94d42d31eae277f055145f8be887062806
Ruben Laguna <ruben.laguna@gmail.com>
2014-11-25 14:45:17 +0100

    Add Powerline fonts

--- create_links_linux.sh
+++ create_links_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # dotfiles in the home dir
+set -e 
+
 ls -A | grep "^\." | grep -v "^\.git$" | grep -v "^\.tmux.conf." | xargs -n1 -I'{}' ln -Tvfs ~/dotfiles/'{}' ~/'{}'
 
 # Fish config files
@@ -17,5 +19,18 @@ ln -Tvfs ~/dotfiles/.gitconfig_linux ~/.gitconfig_platform_specific
 ln -Tvfs ~/dotfiles/.tmux.conf.linux ~/.tmux.conf.extra
 
 # add text
+
+mkdir -pv ~/.vim-tmp
+mkdir -pv ~/.tmp
+mkdir -pv ~/tmp
+
+#Install powerline fonts
+mkdir -pv ~/.fonts
+cp -v ~/dotfiles/fonts/PowerlineSymbols.otf ~/.fonts
+fc-cache -vf ~/.fonts/
+mkdir -pv ~/.config/fontconfig/conf.d
+cp -v ~/dotfiles/fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+
 
 
