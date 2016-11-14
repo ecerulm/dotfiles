38ae8de908379e83e63f97ef112a7cfcd3dfe6f5
Ruben Laguna <ruben.laguna@gmail.com>
2016-11-14 21:00:21 +0100

    emacs: add custom package-archives

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -130,3 +130,23 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 ;;(load "setup-js.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")
+     ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+     ("marmalade" . "https://marmalade-repo.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (magit expand-region tagedit rainbow-delimiters projectile smex ido-ubiquitous cider clojure-mode-extra-font-locking clojure-mode paredit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
