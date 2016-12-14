383e85f12378f73a7f49af72024992e2bfa0d97c
Ruben Laguna <ruben.laguna@gmail.com>
2016-12-14 15:31:57 +0100

    lein: Add cider-nrepl to profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
         :repl-options {:init-ns user}
         :source-paths ["~/.lein/src/"]
-        :plugins [[luminus/lein-template "2.9.10.74"]]}}
+        :plugins [[luminus/lein-template "2.9.10.74"]
+                  [cider/cider-nrepl "0.14.0"]]}}
