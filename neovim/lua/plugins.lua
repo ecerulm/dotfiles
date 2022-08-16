1adff36d45bfd012b25dc5b53acea320a0120f1f
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-16 23:05:22 +0200

    Add nvim-cmp + lspkind

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -9,5 +9,11 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-lualine/lualine.nvim'
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cpm source neovim's builtin LSP
+  use 'hrsh7th/nvim-cmp' -- completion
   use 'neovim/nvim-lspconfig'
+  use 'L3MON4D3/LuaSnip'
+
 end)
