76dbd8261447703d30bf4b4aa89896dc3b378976
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-18 07:52:33 +0200

    nvim: add keymaps

--- neovim/lua/maps.lua
+++ neovim/lua/maps.lua
@@ -1,5 +1,3 @@
-print('maps')
-
 local keymap = vim.keymap
 
 -- Do not yank with x
@@ -12,6 +10,30 @@ keymap.set('n', '-', '<C-x>')
 -- Delete a word backwards
 keymap.set('n', 'dw', 'vb"_d')
 
-
--- Select all 
+-- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save with root permission / does not work switch to suda.vim
+-- https://github.com/neovim/neovim/issues/12103
+-- vim.api.nvim_create_user_command('W', 'w !sudo tee %', {})
+
+-- New tab
+keymap.set('n', 'te', ':tabedit')
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Switch between  windows/splits
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('n', 'sh', '<C-w>h')
+keymap.set('n', 'sk', '<C-w>k')
+keymap.set('n', 'sj', '<C-w>j')
+keymap.set('n', 'sl', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
+--keymap.set('n', 'sb', '<C-w>j20<C-w>_') -- switch to bottom split and set it's height  to 20
