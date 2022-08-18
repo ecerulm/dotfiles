565b5fa5306e1620f46e579d1c4e34a87658507e
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-18 16:45:04 +0200

    nvim: bufferline colors customized for neosolarized

--- neovim/after/plugin/bufferline.rc.lua
+++ neovim/after/plugin/bufferline.rc.lua
@@ -11,7 +11,24 @@ bufferline.setup {
     color_icons = true,
   },
   highlights = {
-
+    separator = {
+      fg = "#073642",
+      bg = "#002b36",
+    },
+    separator_selected = {
+      fg = "#073642",
+    },
+    background = {
+      fg = "#657b83",
+      bg = "#002b36",
+    },
+    buffer_selected = {
+      fg = "#fdf6e3",
+      bold = true,
+    },
+    fill = {
+      bg = "#073642",
+    }
   }
 }
 
