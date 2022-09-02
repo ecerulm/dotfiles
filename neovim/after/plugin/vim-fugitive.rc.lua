51edfc6621e52f4adc8f06cb2e454ae705e531e3
Ruben Laguna <ruben.laguna@gmail.com>
2022-09-02 17:47:53 +0200

    neovim: make :GBrowse work by providing :Browse

--- neovim/after/plugin/vim-fugitive.rc.lua
+++ neovim/after/plugin/vim-fugitive.rc.lua
@@ -1,3 +1,5 @@
 -- vim.g.fugitive_gitlab_domains = {'https://gitlab.mycompany.com', ''}
 
 vim.keymap.set('n', '<Leader>go', '<cmd>GBrowse<cr>', { noremap = true, silent = true })
+vim.keymap.set('v', '<Leader>go', ':GBrowse<cr>', { noremap = true })
+vim.api.nvim_create_user_command('Browse', [[silent execute "!open " .. fnameescape("<args>")]], { nargs = 1 })
