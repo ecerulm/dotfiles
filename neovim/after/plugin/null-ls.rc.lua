0ee2acbdd10f9e7ec36cc2824068a8dfb482af9c
Ruben Laguna <ruben.laguna@gmail.com>
2022-11-28 15:28:43 +0100

    vim: eslint_d -> eslint

--- neovim/after/plugin/null-ls.rc.lua
+++ neovim/after/plugin/null-ls.rc.lua
@@ -5,9 +5,10 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
+    -- null_ls.builtins.diagnostics.eslint_d.with({
+    --   diagnostics_format = '[eslint] #{m}\n(#{c})'
+    -- }),
+    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.trail_space,
     -- null_ls.builtins.formatting.beautysh,
