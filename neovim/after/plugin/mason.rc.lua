6e68388cab316a6db0f385a73e5bd8e456f5e023
Ruben Laguna <ruben.laguna@gmail.com>
2022-11-28 15:28:24 +0100

    vim: mason

--- neovim/after/plugin/mason.rc.lua
+++ neovim/after/plugin/mason.rc.lua
@@ -3,7 +3,15 @@ if (not status) then return end
 local status2, lspconfig = pcall(require, 'mason-lspconfig')
 if (not status2) then return end
 
-mason.setup()
+mason.setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+})
 lspconfig.setup({
   ensure_installed = {
     "sumneko_lua",
