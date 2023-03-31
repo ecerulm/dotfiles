0c3938d1df3184cc7dc007be45f1f94d64e242c5
Ruben Laguna <ruben.laguna@gmail.com>
2023-03-31 12:38:53 +0200

    nvim: Add visual mode <leader>gc to copy git link

--- neovim/after/plugin/vim-fugitive.rc.lua
+++ neovim/after/plugin/vim-fugitive.rc.lua
@@ -3,4 +3,5 @@
 vim.keymap.set('n', '<Leader>go', '<cmd>.GBrowse<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>gc', '<cmd>.GBrowse!<cr>', { noremap = true, silent = true })
 vim.keymap.set('v', '<Leader>go', ':GBrowse<cr>', { noremap = true })
+vim.keymap.set('v', '<Leader>gc', ':GBrowse!<cr>', { noremap = true })
 vim.api.nvim_create_user_command('Browse', [[silent execute "!open " .. shellescape(<q-args>,1)]], { nargs = 1 })
