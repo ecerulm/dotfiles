475313bcd4ae8c166b6ff9aeae959fcaf7957930
Ruben Laguna <ruben.laguna@gmail.com>
2023-09-19 07:53:28 +0200

    Disable lspsaga gp

--- neovim/after/plugin/lspsaga.rc.lua
+++ neovim/after/plugin/lspsaga.rc.lua
@@ -7,5 +7,5 @@ vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', opts)
 -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts) -- this hijacks the digraph feature c-k char1 char2
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
+-- vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts) -- this conflicts with gp (paste and leave cursor at the end of the paste)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
