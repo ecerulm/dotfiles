9926b5022c29c99952b1145ddc5069507c28a952
Rubén Laguna <ruben.laguna@gmail.com>
2023-11-05 12:52:28 +0100

    Add autoformat on save via LSP

--- neovim/init.lua
+++ neovim/init.lua
@@ -12,3 +12,11 @@ if vim.fn.has('macunix') then
 end
 
 vim.cmd("colorscheme gruvbox")
+
+require'lspconfig'.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
