d93f280708b0fef718251ab183482549dbbd8592
Ruben Laguna <ruben.laguna@gmail.com>
2024-01-22 13:03:13 +0100

    neovim: fugitive_gitlab_domains

--- neovim/after/plugin/vim-fugitive.rc.lua
+++ neovim/after/plugin/vim-fugitive.rc.lua
@@ -5,3 +5,5 @@ vim.keymap.set('n', '<Leader>gc', '<cmd>.GBrowse!<cr>', { noremap = true, silent
 vim.keymap.set('v', '<Leader>go', ':GBrowse<cr>', { noremap = true })
 vim.keymap.set('v', '<Leader>gc', ':GBrowse!<cr>', { noremap = true })
 vim.api.nvim_create_user_command('Browse', [[silent execute "!open " .. shellescape(<q-args>,1)]], { nargs = 1 })
+
+-- vim.g.fugitive_gitlab_domains = {'', ''} -- put the private gitlab domains in ~/.config/nvim/init.thismachine.lua
