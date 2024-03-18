c8ef322ab3f00c942f74b97a0a9832d63459a20c
Ruben Laguna <ruben.laguna@gmail.com>
2024-03-18 14:36:28 +0100

    neovim: migrate from packer to lazy.nvim

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,13 +1,74 @@
-local manpath = { -- we overwrite the system $MANPATH because sometime can contain too many path and it takes forever for apropos to give any output
-  "/usr/share/man",
-  "/usr/local/share/man",
-  "/opt/homebrew/share/man",
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- local plugins = require("ecerulm.plugins")
+
+---[[
+local plugins = {
+	{ "morhetz/gruvbox", lazy=false, },
+	{ "lewis6991/gitsigns.nvim" },
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-rhubarb" },
+	{ "shumphrey/fugitive-gitlab.vim" },
+	{ 
+    "nvim-treesitter/nvim-treesitter",
+  },
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "nvim-telescope/telescope-file-browser.nvim" },
+	{
+		"LukasPietzschmann/telescope-tabs",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("telescope-tabs").setup({})
+		end,
+	},
+	{ "mfussenegger/nvim-lint" },
+	{ "tpope/vim-commentary" },
+	{ "kylechui/nvim-surround" },
+	{ "tommcdo/vim-exchange" },
+	{ "dstein64/vim-startuptime" },
+	{ "AndrewRadev/switch.vim" },
+	{ "mhartington/formatter.nvim" },
+	{ "github/copilot.vim" },
+	{ "junegunn/vim-easy-align" },
+	{ "tpope/vim-unimpaired" },
+	{ "williamboman/mason.nvim" },
+	{ "MattesGroeger/vim-bookmarks" },
+	{ "tom-anders/telescope-vim-bookmarks.nvim" },
+	{ "nvim-telescope/telescope-symbols.nvim" },
 }
-vim.env.MANPATH=table.concat(manpath,":") -- let $MANPATH="/usr/share/man" -- :help vim.env
+--]]
 
+require("lazy").setup(plugins, {})
 
+---[[
+local manpath =
+	{ -- we overwrite the system $MANPATH because sometime can contain too many path and it takes forever for apropos to give any output
+		"/usr/share/man",
+		"/usr/local/share/man",
+		"/opt/homebrew/share/man",
+	}
+vim.env.MANPATH = table.concat(manpath, ":") -- let $MANPATH="/usr/share/man" -- :help vim.env
 
-require("ecerulm.plugins")
+-- require("ecerulm.plugins")
 require("ecerulm.base")
 require("ecerulm.highlights")
 require("ecerulm.maps")
@@ -20,13 +81,12 @@ require("ecerulm.formatter") -- configure mhartington/formatter.nvim if it's loa
 require("ecerulm.treesitter") -- configure nvim-treesitter if it's loaded
 require("ecerulm.harpoon2") -- configure telescope.nvim if it's loaded
 
-require('ecerulm.telescope')
-require('telescope').load_extension('vim_bookmarks')
+require("ecerulm.telescope")
+require("telescope").load_extension("vim_bookmarks")
 
 if vim.fn.has("macunix") then
 	require("ecerulm.macos")
 end
-
 
 -- local formatAutoGroup = vim.api.nvim_create_augroup("FormatAutoGroup", {clear = true})
 -- vim.api.nvim_create_autocmd(
@@ -58,3 +118,4 @@ source_file_if_exists(vim.fn.stdpath("config") .. "/init.thismachine.lua")
 
 -- vim.g.fugitive_gitlab_domains = { "gitlab.com" }
 -- vim.cmd("echom g:fugitive_gitlab_domains")
+--]]
