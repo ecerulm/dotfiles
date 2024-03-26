8c13ecab6db09ecd8891c603a1f8ef2ed3db6b46
Ruben Laguna <ruben.laguna@gmail.com>
2024-03-26 12:12:04 +0100

    nvim: treesitter incremental selection

--- neovim/lua/ecerulm/treesitter.lua
+++ neovim/lua/ecerulm/treesitter.lua
@@ -84,6 +84,15 @@ ts.setup {
       },
     },
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
 
 
