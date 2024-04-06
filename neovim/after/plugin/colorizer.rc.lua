a6332eb6706b4dae4baf935bb64dc6cea511a71c
Rubén Laguna <ruben.laguna@gmail.com>
2024-04-06 23:30:04 +0200

    neovim: Remove some after/plugins move gitsigns to lua/ecerulm

--- neovim/after/plugin/colorizer.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, colorizer = pcall(require, 'colorizer')
-if (not status) then return end
-
--- colorize css hex colors in css and javascript files
-colorizer.setup({
-  'css';
-  'javascript';
-
-})
--- neovim/lua/ecerulm/highlights.lua
+++ neovim/lua/ecerulm/highlights.lua
@@ -18,3 +18,4 @@ vim.opt.listchars={
 -- vim.cmd[[highlight Whitespace gui=reverse guifg=#dc322f guibg=none guisp=none]]
 vim.cmd[[match ExtraWhitespace /\s\+$/]]
 vim.cmd[[highlight ExtraWhitespace ctermbg=red guibg=red]]
+vim.cmd[[highlight GitSignsCurrentLineBlame ctermfg=grey ctermbg=darkblue]]
