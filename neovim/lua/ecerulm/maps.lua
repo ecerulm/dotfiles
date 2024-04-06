75f7512fcfcbaa80d80e37f08122c27332ad2d9f
Rubén Laguna <ruben.laguna@gmail.com>
2024-04-06 23:51:50 +0200

    neovim: lsp keymaps

--- neovim/lua/ecerulm/maps.lua
+++ neovim/lua/ecerulm/maps.lua
@@ -133,3 +133,22 @@ keymap.set('v', '<A-k>', ':m \'<-2<CR>gv=gv')
 -- vim-easy-align EasyAlign
 keymap.set('x', 'ga', '<Plug>(EasyAlign)')
 keymap.set('n', 'ga', '<Plug>(EasyAlign)')
+
+
+-- LSP
+keymap.set('n', '<leader>gg', '<cmd>lua vim.lsp.buf.hover()<CR>')
+keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+keymap.set('v', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+keymap.set('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+keymap.set('n', '<leader>gl', '<cmd>lua vim.diagnostic.open_float()<CR>')
+keymap.set('n', '<leader>gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+keymap.set('n', '<leader>gn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+keymap.set('n', '<leader>tr', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+keymap.set('i', '<C-Space>', '<cmd>lua vim.lsp.buf.completion()<CR>')
