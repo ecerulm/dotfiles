84e103f6e7d2553141df8ac7c0ea9764f07585c6
Ruben Laguna <ruben.laguna@gmail.com>
2024-04-09 11:18:32 +0200

    neovim: move telescope keymaps to keymaps.lua and disable telescope file browser extension

--- neovim/lua/ecerulm/keymaps.lua
+++ neovim/lua/ecerulm/keymaps.lua
@@ -4,11 +4,11 @@ local keymap = vim.keymap
 -- keymap.set('n', 'x', '"_x') -- commented out because otherwise xp won't work
 
 -- Increment / decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
 -- keymap.set('n', '<C-a>', 'gg<S-v>G') -- conflicts with increment C-a
@@ -18,53 +18,52 @@ keymap.set('n', 'dw', 'vb"_d')
 -- vim.api.nvim_create_user_command('W', 'w !sudo tee %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "te", ":tabedit")
 
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
 
 -- Switch between  windows/splits / cycle window split
-keymap.set('n', '<Space>', '<C-w>w') -- nnoremap <Space> <C-w>w -- cycle window direction below/right
-keymap.set('n', '<Tab>', '<C-w>w') -- nnoremap <Tab> <C-w>w  -- cycle window direction below/right
-keymap.set('n', '<bs>', '<C-w>W') -- nnoremap <bs> <C-w>W -- cycle windows direction above/left
+keymap.set("n", "<Space>", "<C-w>w") -- nnoremap <Space> <C-w>w -- cycle window direction below/right
+keymap.set("n", "<Tab>", "<C-w>w") -- nnoremap <Tab> <C-w>w  -- cycle window direction below/right
+keymap.set("n", "<bs>", "<C-w>W") -- nnoremap <bs> <C-w>W -- cycle windows direction above/left
 
-keymap.set('n', 'sh', '<C-w>h')
-keymap.set('n', 'sk', '<C-w>k')
-keymap.set('n', 'sj', '<C-w>j')
-keymap.set('n', 'sl', '<C-w>l')
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
 --keymap.set('n', 'sb', '<C-w>j20<C-w>_') -- switch to bottom split and set it's height  to 20
 
-keymap.set('i', 'jk', '<Esc>') -- exit insert mode with jk
+keymap.set("i", "jk", "<Esc>") -- exit insert mode with jk
 
 -- visual model indent
-keymap.set('v', '>', '>gv')
-keymap.set('v', '<', '<gv')
-keymap.set('v', '<Tab>', '>gv')
-keymap.set('v', '<S-Tab>', '<gv')
-
+keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv")
+keymap.set("v", "<Tab>", ">gv")
+keymap.set("v", "<S-Tab>", "<gv")
 
 -- textobjects :help text-objects :help motion
-keymap.set('x', 'ae', ':<c-u>normal! ggVG<cr>', { remap = false, silent = true }) -- visual mode "ae" entire file
-keymap.set('o', 'ae', ':<c-u>normal Vae<cr>', { remap = true, silent = false })   -- ae entire file
+keymap.set("x", "ae", ":<c-u>normal! ggVG<cr>", { remap = false, silent = true }) -- visual mode "ae" entire file
+keymap.set("o", "ae", ":<c-u>normal Vae<cr>", { remap = true, silent = false }) -- ae entire file
 
 -- keymap.set('n', '<Enter>', ':nohlsearch<cr>', {}) -- clear search results , do not remap <Enter> because <Enter> is used for other things like in :h cmdwink
-keymap.set('n', '<c-l>', ':nohlsearch<cr><c-l>', { remap = false })
+keymap.set("n", "<c-l>", ":nohlsearch<cr><c-l>", { remap = false })
 
 -- formatting a file
-keymap.set('n', '<Leader>lf', ':lua vim.lsp.buf.formatting()<cr>', { remap = false, silent = true })
+keymap.set("n", "<Leader>lf", ":lua vim.lsp.buf.formatting()<cr>", { remap = false, silent = true })
 
 -- uppercase with <c-u>
 -- keymap.set('i', '<C-u>', '<Esc>viW~Ea', { remap = false }) -- W and E: spaces separate words, punctuation does not
 -- keymap.set('n', '<C-u>', 'g~iWE', { remap = false }) -- W and E: spaces separate words, punctuation does not
-keymap.set('i', '<C-u>', '<Esc>viw~ea', { remap = false }) -- w and e : punctuation or spaces separate words
-keymap.set('n', '<C-u>', 'g~iwe', { remap = false })       -- w and e : punctuation or spaces separate words
+keymap.set("i", "<C-u>", "<Esc>viw~ea", { remap = false }) -- w and e : punctuation or spaces separate words
+keymap.set("n", "<C-u>", "g~iwe", { remap = false }) -- w and e : punctuation or spaces separate words
 
 -- reselect last visual selection / last pasted text / last changed text
 --
@@ -72,25 +71,23 @@ keymap.set('n', '<C-u>', 'g~iwe', { remap = false })       -- w and e : punctuat
 --   return '`[v`]'
 -- end, { remap = false, expr = true })
 
-keymap.set('n', 'gv', '`[v`]', { remap = false })
-
+keymap.set("n", "gv", "`[v`]", { remap = false })
 
 -- visual mode indent
-keymap.set('v', '>', '>gv')
-keymap.set('v', '<', '<gv')
-keymap.set('v', '<Tab>', '>gv')
-keymap.set('v', '<S-Tab>', '<gv')
-keymap.set('v', '<D-]>', '>gv') -- D- is Cmd- in macOS
-keymap.set('v', '<D-[>', '<gv') -- D- is Cmd- in macOS
+keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv")
+keymap.set("v", "<Tab>", ">gv")
+keymap.set("v", "<S-Tab>", "<gv")
+keymap.set("v", "<D-]>", ">gv") -- D- is Cmd- in macOS
+keymap.set("v", "<D-[>", "<gv") -- D- is Cmd- in macOS
 
 -- normal mode ident
 -- >> and << are builtin
-keymap.set('n', '<D-[', '<<')
-keymap.set('n', '<D-]', '>>')
+keymap.set("n", "<D-[", "<<")
+keymap.set("n", "<D-]", ">>")
 
 -- %% expands to buffer directory
-keymap.set('c', '%%', [[<C-R>=expand('%:h').'/'<cr>]], { remap = false })
-
+keymap.set("c", "%%", [[<C-R>=expand('%:h').'/'<cr>]], { remap = false })
 
 -- the terraform mappings were moved to after/ftplugin/terraform.vim
 
@@ -115,7 +112,6 @@ keymap.set('c', '%%', [[<C-R>=expand('%:h').'/'<cr>]], { remap = false })
 --
 --
 
-
 -- move lines up and down with Alt-j and Alt-k
 --
 --nnoremap <A-j> :m .+1<CR>==
@@ -125,48 +121,75 @@ keymap.set('c', '%%', [[<C-R>=expand('%:h').'/'<cr>]], { remap = false })
 --vnoremap <A-j> :m '>+1<CR>gv=gv
 --vnoremap <A-k> :m '<-2<CR>gv=gv
 --
-keymap.set('n', '<A-j>', ':m .+1<CR>==')
-keymap.set('n', '<A-k>', ':m .-2<CR>==')
-keymap.set('i', '<A-j>', '<esc>:m .+1<CR>==gi')
-keymap.set('i', '<A-k>', '<esc>:m .-2<CR>==gi')
-keymap.set('v', '<A-j>', ':m \'>+1<CR>gv=gv')
-keymap.set('v', '<A-k>', ':m \'<-2<CR>gv=gv')
-
+keymap.set("n", "<A-j>", ":m .+1<CR>==")
+keymap.set("n", "<A-k>", ":m .-2<CR>==")
+keymap.set("i", "<A-j>", "<esc>:m .+1<CR>==gi")
+keymap.set("i", "<A-k>", "<esc>:m .-2<CR>==gi")
+keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- vim-easy-align EasyAlign
-keymap.set('x', 'ga', '<Plug>(EasyAlign)')
-keymap.set('n', 'ga', '<Plug>(EasyAlign)')
-
+keymap.set("x", "ga", "<Plug>(EasyAlign)")
+keymap.set("n", "ga", "<Plug>(EasyAlign)")
 
 -- LSP
-keymap.set('n', '<leader>gg', '<cmd>lua vim.lsp.buf.hover()<CR>')
-keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>')
-keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
-keymap.set('v', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
-keymap.set('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-keymap.set('n', '<leader>gl', '<cmd>lua vim.diagnostic.open_float()<CR>')
-keymap.set('n', '<leader>gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-keymap.set('n', '<leader>gn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-keymap.set('n', '<leader>tr', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-keymap.set('i', '<C-Space>', '<cmd>lua vim.lsp.buf.completion()<CR>')
+keymap.set("n", "<leader>gg", "<cmd>lua vim.lsp.buf.hover()<CR>")
+keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+keymap.set("n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.rename()<CR>")
+keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+keymap.set("v", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+keymap.set("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
+keymap.set("n", "<leader>gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+keymap.set("n", "<leader>gn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+keymap.set("n", "<leader>tr", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+keymap.set("i", "<C-Space>", "<cmd>lua vim.lsp.buf.completion()<CR>")
 
 -- vim-fugitive
 
-vim.keymap.set('n', '<Leader>go', '<cmd>.GBrowse<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>gc', '<cmd>.GBrowse!<cr>', { noremap = true, silent = true })
-vim.keymap.set('v', '<Leader>go', ':GBrowse<cr>', { noremap = true })
-vim.keymap.set('v', '<Leader>gc', ':GBrowse!<cr>', { noremap = true })
-
+vim.keymap.set("n", "<Leader>go", "<cmd>.GBrowse<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>gc", "<cmd>.GBrowse!<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Leader>go", ":GBrowse<cr>", { noremap = true })
+vim.keymap.set("v", "<Leader>gc", ":GBrowse!<cr>", { noremap = true })
 
 -- nvim-cmp
 
 -- keymap.set('i', '<C-Space>', 'cmp.complete()', { expr = true, noremap = true, silent = true })
 
-
 -- Telescope
+--
+local opts = { noremap = true, silent = true }
+keymap.set("n", "<leader>ts", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", { noremap = true, silent = true })
+
+vim.keymap.set(
+	"n",
+	";f",
+	'<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true})<cr>',
+	opts
+)
+vim.keymap.set("n", ";r", '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+vim.keymap.set("n", "\\\\", '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+vim.keymap.set("n", ";t", '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
+vim.keymap.set("n", ";;", '<cmd>lua require("telescope.builtin").resume()<cr>', opts)
+vim.keymap.set("n", ";e", '<cmd>lua require("telescope.builtin").diagnostics()<cr>', opts)
+vim.keymap.set("n", ";m", require('telescope.builtin').marks, opts)
+
+vim.keymap.set(
+	"n",
+	"sf",
+	'<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), respect_git_ignore=false, hidden=true, grouped = true, previewer = false, initial_mode = "normal", layout_config = {height = 40 }})<cr>',
+	opts
+)
+vim.keymap.set(
+	"n",
+	";s",
+	'<cmd>lua require("telescope.builtin").git_status{on_complete = {function() vim.cmd"stopinsert" end }}<cr>',
+	opts
+)
+
+vim.keymap.set("n", ";x", '<cmd>lua require("telescope-tabs").list_tabs()<cr>', opts)
--- neovim/lua/ecerulm/plugins/telescope-nvim.lua
+++ neovim/lua/ecerulm/plugins/telescope-nvim.lua
@@ -27,7 +27,6 @@ return {
 		},
 	},
 	config = function()
-		local builtin = require("telescope.builtin")
 		local actions = require("telescope.actions")
 
 		function telescope_buffer_dir()
@@ -44,56 +43,28 @@ return {
 				},
 			},
 			extensions = {
-				file_browser = {
-					theme = "dropdown",
-					-- disable netrw and use telescope-file-browser instead
-					hijack_netrw = true,
-					mappings = {
-						["i"] = {
-							["C-w"] = function()
-								vim.cmd("normal vbd")
-							end,
-						},
-						["n"] = {
-							["N"] = fb_actions.create,
-							["h"] = fb_actions.goto_parent_dir,
-							["/"] = function()
-								vim.cmd("startinsert")
-							end,
-						},
-					},
-				},
+           -- removed file_browser because you are using nvim-tree
+				-- file_browser = {
+				-- 	theme = "dropdown",
+				-- 	-- disable netrw and use telescope-file-browser instead
+				-- 	hijack_netrw = true,
+				-- 	mappings = {
+				-- 		["i"] = {
+				-- 			["C-w"] = function()
+				-- 				vim.cmd("normal vbd")
+				-- 			end,
+				-- 		},
+				-- 		["n"] = {
+				-- 			["N"] = fb_actions.create,
+				-- 			["h"] = fb_actions.goto_parent_dir,
+				-- 			["/"] = function()
+				-- 				vim.cmd("startinsert")
+				-- 			end,
+				-- 		},
+				-- 	},
+				-- },
 			},
 		})
-		require('telescope').load_extension("file_browser")
-		local opts = { noremap = true, silent = true }
-
-		vim.keymap.set(
-			"n",
-			";f",
-			'<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true})<cr>',
-			opts
-		)
-		vim.keymap.set("n", ";r", '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
-		vim.keymap.set("n", "\\\\", '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
-		vim.keymap.set("n", ";t", '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
-		vim.keymap.set("n", ";;", '<cmd>lua require("telescope.builtin").resume()<cr>', opts)
-		vim.keymap.set("n", ";e", '<cmd>lua require("telescope.builtin").diagnostics()<cr>', opts)
-		vim.keymap.set("n", ";m", builtin.marks, opts)
-
-		vim.keymap.set(
-			"n",
-			"sf",
-			'<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), respect_git_ignore=false, hidden=true, grouped = true, previewer = false, initial_mode = "normal", layout_config = {height = 40 }})<cr>',
-			opts
-		)
-		vim.keymap.set(
-			"n",
-			";s",
-			'<cmd>lua require("telescope.builtin").git_status{on_complete = {function() vim.cmd"stopinsert" end }}<cr>',
-			opts
-		)
-
-		vim.keymap.set("n", ";x", '<cmd>lua require("telescope-tabs").list_tabs()<cr>', opts)
+		-- require('telescope').load_extension("file_browser") -- removed because you are using nvim-tree
 	end,
 }
