1bd3e4b12f5fab46f168058528fd6fc1935c8151
Ruben Laguna <ruben.laguna@gmail.com>
2024-05-20 08:37:59 +0200

    nvim: highlight in terminal mode

--- neovim/lua/ecerulm/highlights.lua
+++ neovim/lua/ecerulm/highlights.lua
@@ -3,3 +3,13 @@
 vim.cmd[[match ExtraWhitespace /\s\+$/]]
 vim.cmd[[highlight ExtraWhitespace ctermbg=red guibg=red]]
 vim.cmd[[highlight GitSignsCurrentLineBlame ctermfg=grey ctermbg=darkblue]]
+
+
+---
+--- Terminal mode
+---
+--
+
+
+vim.cmd[[highlight! link TermCursor Cursor]]
+vim.cmd[[highlight! TermCursorNC guibg=red guifg=white ctermbg=red ctermfg=white]]
