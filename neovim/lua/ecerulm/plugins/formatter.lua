6c54b542028fbeb2df43b0c5210981c09729abe0
Ruben Laguna <ruben.laguna@gmail.com>
2024-06-17 15:26:28 +0200

    nvim: terraform formatter

--- neovim/lua/ecerulm/plugins/formatter.lua
+++ neovim/lua/ecerulm/plugins/formatter.lua
@@ -18,6 +18,7 @@ return {
 				lua = require("formatter.filetypes.lua").stylua,
 				json = require("formatter.filetypes.json").jq,
 				terraform = require("formatter.filetypes.terraform").terraformfmt, -- https://github.com/mhartington/formatter.nvim/blob/master/lua/formatter/filetypes/terraform.lua
+				tf = require("formatter.filetypes.terraform").terraformfmt, -- https://github.com/mhartington/formatter.nvim/blob/master/lua/formatter/filetypes/terraform.lua
 				python = {
 					require("formatter.filetypes.python").black, -- https://github.com/mhartington/formatter.nvim/blob/91651e6afaf6f73b0ffb8b433c06cd4e06f90403/lua/formatter/filetypes/python.lua#L34-L40
 					require("formatter.filetypes.python").isort,
