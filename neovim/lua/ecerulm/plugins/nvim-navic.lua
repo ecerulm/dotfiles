cdfdfe27256fbb03b98b55ca69d2dd99bec230d2
Ruben Laguna <ruben.laguna@gmail.com>
2024-06-17 15:27:00 +0200

    neovim: formatting changes

--- neovim/lua/ecerulm/plugins/nvim-navic.lua
+++ neovim/lua/ecerulm/plugins/nvim-navic.lua
@@ -1,20 +1,19 @@
-
 return {
-  "SmiteshP/nvim-navic",
-  enabled = true,
-  lazy = false,
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-  },
-  config = function()
-    local navic = require("nvim-navic")
+	"SmiteshP/nvim-navic",
+	enabled = true,
+	lazy = false,
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+	},
+	config = function()
+		local navic = require("nvim-navic")
 		local on_attach = function(client, bufnr)
-      if client.server_capabilities.documentSymbolProvider then
-        navic.attach(client, bufnr)
-      end
-    end
-    require("lspconfig").clangd.setup {
-        on_attach = on_attach
-    }
-  end,
+			if client.server_capabilities.documentSymbolProvider then
+				navic.attach(client, bufnr)
+			end
+		end
+		require("lspconfig").clangd.setup({
+			on_attach = on_attach,
+		})
+	end,
 }
