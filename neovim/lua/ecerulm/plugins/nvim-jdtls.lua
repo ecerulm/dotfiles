b767da3ee218b2493962397a15aee11cdd8d4936
Ruben Laguna <ruben.laguna@gmail.com>
2024-06-17 15:29:21 +0200

    nvim: java language server

---
+++ neovim/lua/ecerulm/plugins/nvim-jdtls.lua
@@ -0,0 +1,19 @@
+return {
+	"mfussenegger/nvim-jdtls", -- Eclipse JDT language server , Java language server
+	enabled = true,
+	lazy = false,
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+	},
+				config = function()
+					local jdtls = require("jdtls")
+					local on_attach = function(client, bufnr)
+						if client.server_capabilities.documentSymbolProvider then
+							jdtls.setup_dap()
+						end
+					end
+					require("lspconfig").jdtls.setup({
+						on_attach = on_attach,
+					})
+				end,
+}
