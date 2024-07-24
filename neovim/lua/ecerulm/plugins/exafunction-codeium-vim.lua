c390da1acbd06ad633240c37c599f3e19852b2a4
Ruben Laguna <ruben.laguna@gmail.com>
2024-07-24 15:21:36 +0200

    neovim: codeium.vim always enabled but conditionally disabled

--- neovim/lua/ecerulm/plugins/exafunction-codeium-vim.lua
+++ neovim/lua/ecerulm/plugins/exafunction-codeium-vim.lua
@@ -1,11 +1,10 @@
 return {
-  -- https://github.com/Exafunction/codeium.vim
+	-- https://github.com/Exafunction/codeium.vim
 	"Exafunction/codeium.vim", -- beware codedium.vim and codeium.nvim are different
-	enabled = THISMACHINESETTINGS.codeium_vim_enabled,
+	enabled = true,
+	cond = THISMACHINESETTINGS.codeium_vim_enabled,
 	lazy = false,
-	dependencies = {
-	},
-
+	dependencies = {},
 
 	init = function()
 		vim.g.codeium_log_file = "~/codeium.log"
@@ -13,25 +12,24 @@ return {
 	end,
 
 	config = function()
-		 -- ACCEPT
-		 vim.keymap.set("i", "<M-l>", function()
-		 	return vim.fn["codeium#Accept"]()
-		 end, { expr = true, silent = true })
+		-- ACCEPT
+		vim.keymap.set("i", "<M-l>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
 
-		 -- NEXT SUGGESTION Alt-]
-		 vim.keymap.set("i", "<M-]>", function()
-		 	return vim.fn["codeium#CycleCompletions"](1)
-		 end, { expr = true, silent = true })
+		-- NEXT SUGGESTION Alt-]
+		vim.keymap.set("i", "<M-]>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
 
-		 -- PREVIOUS SUGGESTION Alt-[
-		 vim.keymap.set("i", "<M-[>", function()
-		 	return vim.fn["codeium#CycleCompletions"](-1)
-		 end, { expr = true, silent = true })
+		-- PREVIOUS SUGGESTION Alt-[
+		vim.keymap.set("i", "<M-[>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
 
-		 -- CANCEL C-[ / Escape
-		 vim.keymap.set("i", "<C-[>", function()
-		 	return vim.fn["codeium#Clear"]()
-		 end, { expr = true, silent = true })
-
+		-- CANCEL C-[ / Escape
+		vim.keymap.set("i", "<C-[>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
 	end,
 }
