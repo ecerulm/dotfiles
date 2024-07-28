34760c3800671b5c9aa0ccfd009ae6757a67dcd4
Rubén Laguna <ruben.laguna@gmail.com>
2024-07-28 21:45:46 +0200

    neovim: adjunt indenting rules

--- neovim/after/ftplugin/java.lua
+++ neovim/after/ftplugin/java.lua
@@ -1,1 +1,3 @@
 vim.opt.formatoptions:remove{'c','r','o'} -- has to be in after/ftplugin to run after the system ftplugin/java.vim which sets formatoptions
+
+-- indent related options are set on ~/.config/nvim/indent.java
---
+++ neovim/indent/java.lua
@@ -0,0 +1,7 @@
+--  :help https://neovim.io/doc/user/lua-guide.html#_variables<D-z>o
+--  :help lua-guide-variables
+--  :help 30.3
+vim.b.did_indent = true -- prevents the ~/opt/nvim-macos-arm64/share/nvim/runtime/indent/java.vim
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.cindent = false
