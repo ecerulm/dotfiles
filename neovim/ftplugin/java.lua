b821a6ed9580f486a487ce5ca8fbc9bf8926b111
Rubén Laguna <ruben.laguna@gmail.com>
2024-08-02 07:49:37 +0200

    neovim: lsp keymaps

--- neovim/ftplugin/java.lua
+++ neovim/ftplugin/java.lua
@@ -1,14 +1,16 @@
 local config = {
-    cmd = {'jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-  settings = {
-    java = {
-      configuration = {
-        runtimes = THISMACHINESETTINGS.java_runtimes, -- from ~/.config/nvim/init.thismachine.lua
-        -- echo $(brew --prefix openjdk@21)/libexec/openjdk.jdk/Contents/Home
-      },
-    },
-  },
+	cmd = { "jdtls" },
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	settings = {
+		java = {
+			configuration = {
+				runtimes = THISMACHINESETTINGS.java_runtimes, -- from ~/.config/nvim/init.thismachine.lua
+				-- echo $(brew --prefix openjdk@21)/libexec/openjdk.jdk/Contents/Home
+			},
+			symbols = {
+				includeSourceMethodDeclarations = true,
+			},
+		},
+	},
 }
-require('jdtls').start_or_attach(config)
-
+require("jdtls").start_or_attach(config)
