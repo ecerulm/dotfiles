cf0e8a9234996711c3d562b90dd16b9d21e2e5a6
Rubén Laguna <ruben.laguna@gmail.com>
2024-12-05 20:03:32 +0100

    nvim-treesitter-context dev=false

--- neovim/lua/ecerulm/plugins/nvim-treesitter-context.lua
+++ neovim/lua/ecerulm/plugins/nvim-treesitter-context.lua
@@ -1,16 +1,15 @@
 return {
-  -- https://github.com/nvim-treesitter/nvim-treesitter-context
-  -- show at the top of the buffer where are you. Shows the first line
-  --  of the class , first line of the  method , first line of the if , etc
+	-- https://github.com/nvim-treesitter/nvim-treesitter-context
+	-- show at the top of the buffer where are you. Shows the first line
+	--  of the class , first line of the  method , first line of the if , etc
 	"nvim-treesitter/nvim-treesitter-context",
 	enabled = true,
+	dev = false,
 	cond = true,
 	lazy = false,
 	dependencies = {
 		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		"nvim-treesitter/nvim-treesitter",
 	},
-  opts={
-    mode = "cursor",
-  },
+	opts = {},
 }
