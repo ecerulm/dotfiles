b1764b7ebecab2939497b49ddc0a04ed49648847
Ruben Laguna <ruben.laguna@gmail.com>
2024-12-20 08:50:23 +0100

    nvim: add aerial.nvim

---
+++ neovim/lua/ecerulm/plugins/aerial-nvim.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/aerial.nvim",
+	-- Optional dependencies
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+		-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+		on_attach = function(bufnr)
+			-- Jump forwards/backwards with '{' and '}'
+			vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+		end,
+		layout = {
+			max_width = { 80, 0.4 },
+		},
+	},
+	config = function(_, opts)
+		require("aerial").setup(opts)
+		vim.keymap.set("n", "<leader>zz", "<cmd>AerialToggle!<CR>")
+	end,
+}
