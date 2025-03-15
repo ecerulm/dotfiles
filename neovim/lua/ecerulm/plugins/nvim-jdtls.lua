da67d679d3304eb12146f6db2334308bbab697be
Rubén Laguna <ruben.laguna@gmail.com>
2025-03-15 21:13:47 +0100

    vim: borders for lsp hover and signature help

--- neovim/lua/ecerulm/plugins/nvim-jdtls.lua
+++ neovim/lua/ecerulm/plugins/nvim-jdtls.lua
@@ -8,14 +8,5 @@ return {
 		{ "neovim/nvim-lspconfig" },
 	},
 	config = function()
-		-- local jdtls = require("jdtls")
-		-- local on_attach = function(client, bufnr)
-		-- 	if client.server_capabilities.documentSymbolProvider then
-		-- 		jdtls.setup_dap()
-		-- 	end
-		-- end
-		-- require("lspconfig").jdtls.setup({
-		-- 	on_attach = on_attach,
-		-- })
 	end,
 }
