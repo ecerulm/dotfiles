5ed4af37df4deca77c8672e72c61684403f1810c
Rubén Laguna <ruben.laguna@gmail.com>
2025-03-16 20:11:35 +0100

    neovim: jdtls allow gradle jar checksum

--- neovim/ftplugin/java.lua
+++ neovim/ftplugin/java.lua
@@ -1,6 +1,27 @@
 local config = {
 	cmd = { "jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	init_options = {
+		settings = {
+			java = {
+				implementationsCodeLens = { enabled = true },
+				imports = {
+					gradle = {
+						enabled = true,
+						wrapper = {
+							enabled = true,
+							checksums = {
+								{
+									sha256 = "81a82aaea5abcc8ff68b3dfcb58b3c3c429378efd98e7433460610fecd7ae45f",
+									allowed = true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 	settings = {
 		java = {
 			configuration = {
