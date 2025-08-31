da2202eba0eeb7fda2f86c287e47f267244b9f74
Rubén Laguna <ruben.laguna@gmail.com>
2025-08-31 09:24:03 +0200

    vim: simplify LSP setup

--- neovim/lua/ecerulm/plugins/mason-lspconfig.lua
+++ neovim/lua/ecerulm/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
 	-- https://github.com/williamboman/mason-lspconfig.nvim
 	"williamboman/mason-lspconfig.nvim",
-	enabled = true,
+	enabled = false,
 	lazy = false,
 	dependencies = {
 		"williamboman/mason.nvim",
--- neovim/lua/ecerulm/plugins/snacks-nvim.lua
+++ neovim/lua/ecerulm/plugins/snacks-nvim.lua
@@ -79,10 +79,14 @@ return {
 		-- LSP
 		{ "gd",              function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
 		{ "gD",              function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
-		{ "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+		-- { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+    -- gr conflicts with nvim default grn for rename, and gra, grr, grt, grt
+		{ "grr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+
 		{ "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
 		{ "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
 		{ "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
+		{ "gO",              function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" }, -- overrices nvim gO https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#lsp_symbols
 		{ "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
 		-- Other
 		{ "<leader>z",       function() Snacks.zen() end,                                            desc = "Toggle Zen Mode" },
