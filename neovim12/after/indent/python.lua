ff21e662939efc10bb750aa63722336e1a0d4943
Rubén Laguna <ruben.laguna@gmail.com>
2026-01-13 11:11:55 +0100

    Use default indentexpr for python

--- neovim12/after/indent/python.lua
+++ neovim12/after/indent/python.lua
@@ -1,5 +1,6 @@
 vim.opt_local.autoindent = true
 vim.opt_local.smartindent = true
 -- vim.opt_local.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-vim.opt_local.indentexpr = ""
+vim.opt_local.indentexpr = "python#GetIndent(v:lnum)" -- bring back the default python indent
+-- vim.opt_local.indentexpr = ""
 vim.treesitter.start()
