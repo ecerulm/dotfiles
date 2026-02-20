c491388b9e6c87202cb16d0da3f30c4a0780d1a2
Rubén Laguna <ruben.laguna@gmail.com>
2026-02-20 22:21:37 +0100

    Adjust the default for python

--- neovim/after/indent/python.lua
+++ neovim/after/indent/python.lua
@@ -1,8 +1,5 @@
--- vim.opt_local.autoindent = false -- autoindent noautoindent
--- vim.opt_local.smartindent = false -- smartindent nosmartindent
--- vim.opt_local.cindent = false -- cindent nocindent
--- vim.opt_local.indentexpr = "" -- on after/ftplugin/python.lua
+vim.opt_local.autoindent = false -- autoindent noautoindent
+vim.opt_local.smartindent = false -- smartindent nosmartindent
+vim.opt_local.cindent = true -- cindent nocindent
 
--- vim.opt_local.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
--- vim.opt_local.indentexpr = "python#GetIndent(v:lnum)" -- bring back the default python indent
--- vim.opt_local.indentexpr = ""
+-- indentexpr is set in ~/.config/nvim/after/ftplugin/python.lua
